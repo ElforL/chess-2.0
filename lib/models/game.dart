d97801a940b74fbf3e80d333a9baca5a83ee1538
Laith Shono <elfordev@gmail.com>
2022-07-18 22:24:40 +0300

    🚧 Add WIP `ChessGame` class

---
+++ lib/models/game.dart
@@ -0,0 +1,22 @@
+import 'package:chess_2/models/board.dart';
+
+import 'spot.dart';
+
+class ChessGame {
+  ChessBoard _board;
+  ChessGame() : _board = ChessBoard();
+
+  void movePiece(SpotCoordinates start, SpotCoordinates end) {
+    final startSpot = _board.getSpotFromCoords(start);
+    if (startSpot.piece == null) return;
+    final endSpot = _board.getSpotFromCoords(end);
+    final piece = startSpot.piece;
+
+    final isLegalMove = piece!.isLegalMove(end, _board);
+
+    if (isLegalMove) {
+      endSpot.piece = piece;
+      startSpot.piece = null;
+    }
+  }
+}

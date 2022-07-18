47ae2362dc93d9fff435dc30cbb95360b6bddc4b
Laith Shono <elfordev@gmail.com>
2022-07-18 22:23:34 +0300

    ♟ Add Elephant 🐘

---
+++ lib/models/pieces/elephant.dart
@@ -0,0 +1,23 @@
+import '../board.dart';
+import '../spot.dart';
+import 'piece.dart';
+
+class Elephant extends ChessPiece {
+  Elephant(super.coordinates, super.side);
+
+  @override
+  bool isLegalMove(SpotCoordinates coordinates, ChessBoard board) {
+    if (!isValidNewSpot(coordinates)) return false;
+    final endSpot = board.getSpotFromCoords(coordinates);
+
+    // can move if there's no piece or the piece isn't a teammate
+    return !endSpot.hasPiece || endSpot.piece?.side != side;
+  }
+
+  @override
+  bool isValidNewSpot(SpotCoordinates coordinates) {
+    if ((coordinates.x - x).abs() != 2) return false;
+    if ((coordinates.y - y).abs() != 2) return false;
+    return true;
+  }
+}
